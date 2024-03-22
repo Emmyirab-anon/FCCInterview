0158d8cdcc35e7acddcc4cd459e3248b21071dd1
EmmanuelDevpro <132281697+EmmanuelDevpro@users.noreply.github.com>
2024-03-22 14:20:21 -0600

    Initial commit

---
+++ src/main/java/org/example/Game.java
@@ -0,0 +1,86 @@
+package org.example;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.Random;
+
+public class Game {
+	private Map<Integer, Integer> scoreCount = new HashMap<>();
+	private Integer iterations = 0;
+	private Integer diceAmount = 0;
+	private Integer originalDiceAmount = 0;
+	private double timeToRunSImulation = 0;
+
+	Game(Integer iterations, Integer diceAmount) {
+		this.iterations = iterations;
+		this.diceAmount = diceAmount;
+		this.originalDiceAmount = diceAmount;
+	}
+
+	public void play() {
+		long startTime = System.nanoTime();
+
+		for (int i = 0; i < iterations; i++) {
+			int score = 0;
+			while (!gaveOver()) {
+				List<Integer> result = roleDice();
+				if (result.contains(3)) {
+					int amountofThrees = Collections.frequency(result, 3);
+					removeDie(amountofThrees);
+				} else {
+					score += result.get(0);
+					removeDie(1);
+				}
+			}
+			updateScoreCount(score);
+			resetGame();
+		}
+
+		long endTime = System.nanoTime();
+		timeToRunSImulation = ((endTime - startTime) / 1_000_000_000.0);
+		printResult();
+	}
+
+
+	private void removeDie(Integer diceAmount) {
+		this.diceAmount -= diceAmount;
+	}
+
+	private void resetGame() {
+		this.diceAmount = originalDiceAmount;
+	}
+
+	private List<Integer> roleDice() {
+		List<Integer> result = new ArrayList<>();
+		Random random = new Random();
+		for (int i = 0; i < diceAmount; i++) {
+			result.add(random.nextInt(6) + 1);
+		}
+		return result.stream().sorted().toList();
+	}
+
+	private boolean gaveOver() {
+		return diceAmount < 1;
+	}
+
+	private void updateScoreCount(int score) {
+		if (scoreCount.containsKey(score)) {
+			scoreCount.put(score, scoreCount.get(score) + 1);
+		} else {
+			scoreCount.put(score, 1);
+		}
+	}
+
+	private void printResult() {
+		System.out.printf("Number of simulations was %d using %d dice.%n", iterations, originalDiceAmount);
+		scoreCount.forEach((k, v) -> {
+			System.out.printf("Total %d occurs %d occurred %d times.%n", k, v / 100, v);
+		});
+
+		System.out.println("Total simulation took " + String.format("%.1f", timeToRunSImulation) + " seconds.");
+	}
+
+}
---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,8 @@
+package org.example;
+
+public class Main {
+	public static void main(String[] args) {
+		Game newGame = new Game(10000, 5);
+		newGame.play();
+	}
+}
